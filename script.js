c6a91d0fa55db9b7560e6d1d57032007bc53775b
Malek Fhima <malekfhima1@gmail.com>
2025-01-19 16:39:00 +0100

    malek

--- script.js
+++ script.js
@@ -25,7 +25,7 @@ function verifMat(ch) {
     let ns = ch.slice(0, pt);
     let p = ch.slice(pt, pt + 3);
     let nv = ch.slice(pt + 3);
-    if (p.toUpperCase() === "TUN" && parseInt(nv) >= 1 && parseInt(nv) < 10000 && parseInt(ns) >= 1 && parseInt(ns) < 246) {
+    if (p.toUpperCase() === "TUN" && parseInt(nv) >= 1 && parseInt(nv) < 10000 && parseInt(ns) >= 1 && parseInt(ns) < 249) {
         return true;
     } else {
         return false;
