5d0a2aebe92dac2601037a7d86dfdeec6a4f7c95
Malek Fhima <malekfhima1@gmail.com>
2025-01-26 17:55:16 +0100

    malek

--- enregister/main.php
+++ enregister/main.php
@@ -1,8 +1,17 @@
 <?php
-include("../conx.php");
+include("../cnx.php");
 extract($_POST);
-$dat_dep=date("yyyy-MM-dd", strtotime(now));
-
-$res=mysqli_query($cnx,"INSERT INTO liste_vc VALUES ('mat','cc','nc','dur','$dat_dep','cch','NULL','NULL')");
-
+$d = date("Y-m-d");
+$resmt= mysqli_query($cnx , "SELECT prix from voiture where mat = '$mat'");
+$p = mysqli_fetch_array($resmt)[0];
+$mt = $p * $dur;
+$reset =mysqli_query($cnx , "SELECT * from liste_vc where mat = '$mat'");
+$etat = "";
+if(mysqli_num_rows($reset) === 1){
+    $etat = "mosh mawjouda tw !";
+}else{
+    $etat = "mawjouda!";
+}
+$res = mysqli_query($cnx ,"INSERT into liste_vc (mat,cc,nc,nb_j,date_dep,c_ch,m_t,etat) values('$mat','$cc','$nc','$dur','$d','$cch','$mt','$etat')");
+mysqli_close($cnx);
 ?>
